ba6c4c3d07d16f3ee9f4de53907ae3cef5354814
veri-tty <verity@cock.li>
2025-05-04 22:01:32 +0200

    uhhhh

--- modules/servarr.nix
+++ modules/servarr.nix
@@ -16,13 +16,30 @@
   config = {
     modules = [
       inputs.nixarr.nixosModules.default
-    ]
-    nixarr = {
+    ];
+    nixarr = lib.mkIf config.library.enable {
       enable = true;
+      stateDir = "/mnt/dockerdata/servarr/";
       vpn = {
         enable = true;
         wgConf = "/placeholder/for/wg.conf";
-      }
+
+      };
+      readarr = {
+        enable = true;
+      };
+      transmission = {
+        enable = true;
+        flood.enable = true;
+        privateTrackers.cross-seed.enable = true;
+        vpn.enable = true;
+      };
+    };
+    services = lib.mkIf config.library.enable {
+      calibre-web = {
+        enable = true;
+        options.enableKepubify = true;
+      };
     };
   };
 }
