fdc1d5fc009bc85df724096fe4a2575fc7c9792d
veri-tty <mattescantcode@tutanota.com>
2025-05-20 18:29:54 +0200

    :(

--- modules/servarr.nix
+++ modules/servarr.nix
@@ -20,28 +20,9 @@
     nixarr = lib.mkIf config.library.enable {
       enable = true;
       stateDir = "/mnt/dockerdata/servarr/";
-      vpn = {
-        enable = true;
-        wgConf = "/placeholder/for/wg.conf";
-
-      };
       readarr = {
         enable = true;
       };
-      transmission = {
-        enable = true;
-        flood.enable = true;
-        #privateTrackers.cross-seed.enable = true;
-        vpn.enable = true;
-      };
-      prowlarr.enable = true;
-    };
-    services = lib.mkIf config.library.enable {
-      calibre-web = {
-        enable = true;
-        options.enableKepubify = true;
-      };
-      calibre-server.enable = true;
     };
   };
 }
