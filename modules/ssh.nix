1b411e1d9b2df390aaab812d8d7a267efc5dee10
ml <mattescantcode@tutanota.com>
2025-05-22 14:54:39 +0200

    new strelok pubkey

--- modules/ssh.nix
+++ modules/ssh.nix
@@ -23,7 +23,7 @@
     users.users.ml.openssh.authorizedKeys.keys = [
       # Add your public key here
       "ssh-ed25519 AAAAC3NzaC1lZDI1NTE5AAAAIKscmbcptGav35eypKUFdRdsnnTqNH4d9xtr7SykyoQJ ml@fitz"
-
+      "ssh-ed25519 AAAAC3NzaC1lZDI1NTE5AAAAINKTxlRrPhGZnHKJZpcjLcqr1Vvod2rzZVEjgHD9oI93 ml@strelok"
       "ssh-ed25519 AAAAC3NzaC1lZDI1NTE5AAAAIC/U51AA+viYJACfTPeHN/P7Prl7iqOuigtPxNvCgzX/ ml@roamer"
     ];
   };
